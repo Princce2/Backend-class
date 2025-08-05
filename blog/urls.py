16cec9703bc5cfe03d441712c776aa3fef4b272a
Princce2 <princeosawaru5@gmail.com>
2025-08-05 10:01:14 +0100

    hello

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, detail
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='index'),
+    path('article/<int:id>/', detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
+
+"""
+ModelName.objects.all() --> all data
+ModelName.objects.filter(user=user) --> filter by user
+get_object_or_404(Article, id=article_id) --> get object or 404
+ModelName.objects.filter(user=user).first() --> get first object
+"""
 
 def index(request):
-    return render(request, 'index.html')
+    articles = Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+
+def detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, 'index.html', {'article': article})
+
+
